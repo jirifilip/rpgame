8f89ac16e56faa2f40132926c250933e1637317c
Jiří Filip <j.f.ilip@seznam.cz>
2023-09-29 13:23:44 +0200

    Refactor test package structure

--- tests/rpgame/conftest.py
+++ tests/rpgame/conftest.py
@@ -2,10 +2,14 @@ from rpgame.entities import Player
 from rpgame.text_ui import TextUI
 
 
-from typing import Tuple
+from typing import List, Tuple, Type
 
 
-def _create_world_with_player(initial_state: str) -> Tuple[TextUI, Player]:
+def create_world_with_player(initial_state: str) -> Tuple[TextUI, Player]:
     player = Player(0, 0)
     board = TextUI(initial_state, entities=[player])
     return board, player
+
+
+def filter_object_type(objects: List[object], desired_type: Type):
+    return [o for o in objects if type(o) == desired_type   ]
---
+++ tests/rpgame/test_text_ui/test_from_map.py
@@ -0,0 +1,65 @@
+import pytest
+
+from rpgame.entities import Wall, Player
+from rpgame.text_ui import TextUI
+from tests.rpgame.conftest import create_world_with_player, filter_object_type
+
+
+@pytest.fixture
+def larger_map():
+    yield (
+"""
+|-----|
+| @   |
+|     |
+|-----|
+"""
+    )
+    
+    
+@pytest.fixture
+def small_map():
+    yield (
+"""
+ _
+| |
+|@|
+|_|
+"""   
+    )
+
+    
+def test_player_can_move_right(larger_map):
+    board, player = create_world_with_player(larger_map)
+    
+    player.move(1)
+    player.move(0, 1)
+    
+    expected_board = (
+"""
+|-----|
+|     |
+| @   |
+|-----|
+"""
+    )
+    assert board.render() == expected_board    
+    
+    
+def test_can_be_initialized(larger_map):
+    ui = TextUI.from_map(larger_map)
+    
+    assert len(ui.entities) == 9
+    
+    
+def test_text_ui_has_correct_entity_counts_when_initialized_with_map(small_map):
+    ui = TextUI.from_map(small_map)
+    
+    walls = filter_object_type(ui.entities, Wall)
+    players = filter_object_type(ui.entities, Player)
+    assert len(list(walls)) == 8
+    assert len(list(players)) == 1    
+
+
+def test_entities_have_correct_positions_when_initialized_with_map():
+    pass
