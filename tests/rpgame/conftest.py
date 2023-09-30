7689989c936bd62930507ea08c3b906877045431
Jiří Filip <j.f.ilip@seznam.cz>
2023-09-30 20:19:43 +0200

    Reorganize test code

--- tests/rpgame/conftest.py
+++ tests/rpgame/conftest.py
@@ -9,20 +9,6 @@ from typing import List, Tuple, Type
 from rpgame.game import Game
 
 
-
-def create_game(text_map: str) -> Tuple[Room, Game]:
-    room = Room.from_text_map(text_map)
-
-    players = filter_entity_type(room.entities, Player)
-    player_facade = Game(players[0], room.entities)
-    
-    return room, player_facade
-
-
-def filter_entity_type(objects: List[Entity], desired_type: Type) -> List[Entity]:
-    return [o for o in objects if type(o) == desired_type ]
-
-
 @pytest.fixture
 def small_map():
     yield (
@@ -45,9 +31,22 @@ def larger_map():
 |||||||
 """
     )
+    
+
+def create_game(text_map: str) -> Tuple[Room, Game]:
+    room = Room.from_text_map(text_map)
+
+    players = filter_entity_type(room.entities, Player)
+    player_facade = Game(players[0], room.entities)
+    
+    return room, player_facade
 
 
 def assert_entities_are_same_by_location(actual_entities: List[Entity], expected_entities: List[Entity]):
     """Compares expected entities with actual entities by their location"""
     for actual, expected in zip(actual_entities, expected_entities):
         assert (actual.left, actual.top) == (expected.left, expected.top)
+        
+
+def filter_entity_type(objects: List[Entity], desired_type: Type) -> List[Entity]:
+    return [o for o in objects if type(o) == desired_type ]
