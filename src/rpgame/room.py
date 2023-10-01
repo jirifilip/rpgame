9db6c543e0c503131acc95303e4136a4392ff878
Jiří Filip <j.f.ilip@seznam.cz>
2023-10-01 20:53:06 +0200

    Refactoring entity factories

--- src/rpgame/room.py
+++ src/rpgame/room.py
@@ -9,7 +9,11 @@ class Room:
     dimensions: Tuple[int, int]
     entities: List[Entity]
 
-    _CHARACTER_TO_ENTITY_FACTORY = {"@": Player, "|": Wall, "o": Gold}
+    _CHARACTER_TO_ENTITY_FACTORY = {
+        "@": lambda left, top: Player(left, top),
+        "|": lambda left, top: Wall(left, top),
+        "o": lambda left, top: Gold(left, top),
+    }
 
     @classmethod
     def from_text_map(cls, text_map: str):
@@ -29,11 +33,11 @@ class Room:
 
     @staticmethod
     def _convert_text_map_to_entities(text_map_lines: List[str]) -> List[Entity]:
-        factory_map = Room._CHARACTER_TO_ENTITY_FACTORY
+        character_to_entity_factory = Room._CHARACTER_TO_ENTITY_FACTORY
 
         return [
-            factory_map[character](left_idx, top_idx)
+            character_to_entity_factory[character](left_idx, top_idx)
             for top_idx, row in enumerate(text_map_lines)
             for left_idx, character in enumerate(row)
-            if character in factory_map
+            if character in character_to_entity_factory
         ]
